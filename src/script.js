5c903e72d66af0c92469f8300612b6faf8530f1a
Szymon Orchowski <szymonorchowski@gmail.com>
2022-04-10 16:51:23 +0100

    URL query added

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import Presentation from './Presentation/Presentation'
 
 const parts = []
-if(document.URL.split("?")[1].split("=")[1].split(",")) {
+if(document.URL.includes("?")) {
     document.URL.split("?")[1].split("=")[1].split(",").forEach(part => {
         parts.push(part)
     })
